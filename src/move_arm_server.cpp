eb7f4b075d3fd7b2e31271bb82a85b98dd8f3e83
red1laref <red1.laref@gmail.com>
2017-07-27 09:21:48 +0900

    removed agile grasp from the package

--- src/move_arm_server.cpp
+++ src/move_arm_server.cpp
@@ -3,10 +3,8 @@
 ////
 
 #include <move_arm_server.h>
-#include <agile_grasp/Grasps.h>
 #include <Eigen/Dense>
 #include <eigen_conversions/eigen_msg.h>
-#include <grasp_selection/SelectGrasps.h>
 
 // taken from the grasp selection package
 
@@ -43,44 +41,6 @@ bool move_joint( wm_moveit_server::move_jointsRequest &req, wm_moveit_server::mo
     return true;
 
 }
-
-
-bool grasp( wm_moveit_server::pickRequest &req, wm_moveit_server::pickResponse &resp ) {
-    moveit::planning_interface::MoveGroupInterface group(req.move_group);
-    bool success = false;
-    group.allowReplanning(true);
-    group.setNumPlanningAttempts(2);
-    moveit::planning_interface::MoveGroupInterface::Plan plan;
-
-    // call the grasp_selection service
-    grasp_selection::SelectGrasps selector;
-    selector.request.hand_pose = group.getCurrentPose().pose;
-    ros::service::waitForService( "grasp_selection" );
-    ros::service::call( "grasp_selection", selector );
-    selector.response.grasps.grasps[0].pose;
-
-
-
-    int length = (int) selector.response.grasps.grasps.size();
-    for (int i = 0; i < length; i++) {
-
-        group.setPoseTarget( selector.response.grasps.grasps[i].pose );
-
-        if (group.plan(plan).val == 1) {
-            success = true;
-            break;
-        }
-    }
-    if (success) {
-        group.asyncExecute(plan);
-        waitForExecution(group);
-    }
-    resp.success = (char)(success ? 1 : 0);
-
-    return true;
-}
-
-
 
 
 
@@ -131,42 +91,12 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
 
     ros::ServiceServer serviceMove = nh.advertiseService( "move_arm", move );
-    ros::ServiceServer servicePick = nh.advertiseService( "pick", grasp );
     ros::ServiceServer serviceMoveJoints = nh.advertiseService( "move_joints", move_joint);
     ROS_INFO("Ready to move.");
     //ros::spin();
     while ( ros::ok()){}
 
     return 0;
-}
-
-
-
-
-
-
-
-
-tf::Quaternion calculateHandOrientations(const GraspEigen& grasp)
-{
-    // calculate first hand orientation
-    Eigen::Matrix3d R = Eigen::MatrixXd::Zero(3, 3);
-    R.col(0) = -1.0 * grasp.approach_;
-    R.col(1) = grasp.axis_;
-    R.col(2) << R.col(0).cross(R.col(1));
-
-    // rotate by 180deg around the grasp approach vector to get the "opposite" hand orientation
-    Eigen::Transform<double, 3, Eigen::Affine> T(Eigen::AngleAxis<double>(3.14159, grasp.approach_));
-
-    // convert Eigen rotation matrices to TF quaternions and normalize them
-    tf::Matrix3x3 TF1;
-    tf::matrixEigenToTF(R, TF1);
-
-    tf::Quaternion quat1;
-//    TF1.getRotation(quat1);
-    quat1.normalize();
-
-    return quat1;
 }
 
 void waitForExecution( moveit::planning_interface::MoveGroupInterface group ){
